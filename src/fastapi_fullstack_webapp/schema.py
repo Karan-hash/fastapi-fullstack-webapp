f6832d6a8658b7d042bca472b3d858e661c3ddb0
Karan-hash <karankaushal907@gmail.com>
2026-08-08 22:09:33 +0530

    Implemented CRUD update and delete operations for users and posts.

--- src/fastapi_fullstack_webapp/schema.py
+++ src/fastapi_fullstack_webapp/schema.py
@@ -19,6 +19,10 @@ class UserResponse(UserBase):
     image_file: str | None
     image_path: str
 
+class UserUpdate(UserBase):
+    username: str| None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -36,3 +40,7 @@ class PostResponse(PostBase):
     user_id: int
     date_posted: datetime
     author: UserResponse
+
+class PostUpdate(PostBase):
+    title: str | None = Field(default=None, min_length=1, max_length=100)
+    content: str | None = Field(default=None, min_length=1)
